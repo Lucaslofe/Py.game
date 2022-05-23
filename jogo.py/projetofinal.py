9d634b712782dd1db35c82c42bae093910f6e15d
leonardopaloschi <leonardopaloschi@outlook.com>
2022-05-23 11:44:46 -0300

    tela inicial

--- jogo.py/projetofinal.py
+++ jogo.py/projetofinal.py
@@ -5,17 +5,21 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption('Vai corinthians')
+window = pygame.display.set_mode((1100, 650))
+pygame.display.set_caption('py.zza game')
 
 # ----- Inicia estruturas de dados
 game = True
 
 # ----- Inicia assets
-font = pygame.font.SysFont(None, 48)
-text = font.render('o palmeiras não tem mundial', True, (0, 255, 0))
-
+font = pygame.font.SysFont(None, 60)
+text = font.render('py.zza game', True, (0, 255, 0))
+font = pygame.font.SysFont(None, 42)
+text2 = font.render('press any button to start', True, (255, 0, 0))
+image = pygame.image.load("pizza.png").convert_alpha()
+image = pygame.transform.scale(image, (300, 300))
 # ===== Loop principal =====
+
 while game:
     # ----- Trata eventos
     for event in pygame.event.get():
@@ -25,8 +29,12 @@ while game:
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branca
-    window.blit(text, (10, 10))
-
+    window.blit(text, (420, 40))
+    window.blit(image,(400,100))
+    window.blit(text2, (380, 500))
+    #cor = (0,255,0)
+    #vertices = [(400,400),(700,400),(700,500),(400,500)]
+    #pygame.draw.polygon(window,cor,vertices)
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
