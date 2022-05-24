a959874835d1bb28f52be85ca0b0552f98aed97c
leonardopaloschi <leonardopaloschi@outlook.com>
2022-05-24 19:44:48 -0300

    add ingredients

--- jogo.py/projetofinal.py
+++ jogo.py/projetofinal.py
@@ -30,6 +30,26 @@ clock = pygame.time.Clock()
 fps = 30
 image4 = pygame.image.load("pizzasr.png").convert_alpha()
 image4 = pygame.transform.scale(image4, (200, 200))
+madeira = pygame.image.load("madeira.png").convert_alpha()
+madeira = pygame.transform.scale(madeira,(1100,200))
+duende = pygame.image.load("duende.png").convert_alpha()
+duende = pygame.transform.scale(duende, (1,1))
+barata = pygame.image.load("barata.png").convert_alpha()
+barata = pygame.transform.scale(barata, (1,1))
+sapo = pygame.image.load("sapo.png").convert_alpha()
+sapo = pygame.transform.scale(sapo, (1,1))
+slime = pygame.image.load("slime.png").convert_alpha()
+slime = pygame.transform.scale(slime, (1,1))
+zumbi = pygame.image.load("zumbi.png").convert_alpha()
+zumbi = pygame.transform.scale(zumbi, (1,1))
+bota = pygame.image.load("bota.png").convert_alpha()
+bota = pygame.transform.scale(bota, (1,1))
+chip = pygame.image.load("chip.png").convert_alpha()
+chip = pygame.transform.scale(chip, (1,1))
+pimenta = pygame.image.load("pimenta.png").convert_alpha()
+pimenta = pygame.transform.scale(pimenta, (1,1))
+
+
 # ===== Loop principal =====
 state = "start_screen"
 vel_esteira = 5
@@ -55,6 +75,7 @@ while game:
         window.blit(image3,(0,0))
         window.blit(esteira,(x_esteira,400))
         window.blit(esteira,(x_esteira2,400))
+        window.blit(madeira,(0,200))
         x_esteira += vel_esteira
         x_esteira2 += vel_esteira
         x_pizza += vel_esteira
@@ -63,6 +84,8 @@ while game:
         if x_esteira2 >= 1100:
             x_esteira2 = -1900
         window.blit(image4,(x_pizza,425))
+        if x_pizza > 1100:
+            x_pizza = -200
         
 
 
@@ -78,3 +101,5 @@ while game:
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+#peperoni, palmito, queijo, milho, ervilha, cebola,   
