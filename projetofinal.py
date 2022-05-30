c3feb4047afac6ae7ce6322cfe7c9bce1f2fe23f
Joao Palma <joaoogp@al.insper.edu.br>
2022-05-30 19:52:30 -0300

    Adição de classes, criação de sprites, correção para que os ingredientes fiquem juntos à pizza, eliminação de drag function

--- projetofinal.py
+++ projetofinal.py
@@ -1,7 +1,10 @@
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
 import pygame
+from config import *
 from sqlalchemy import false
+
+from sprites import Ingrediente, Pizza
 
 pygame.init()
 # ----- Gera tela principal
@@ -16,13 +19,13 @@ game = True
 drag_duende, drag_barata, drag_sapo, drag_slime, drag_zumbi, drag_chip, drag_pimenta, drag_bota = False, False, False, False, False, False, False, False
 # ----- Inicia assets
 font = pygame.font.SysFont(None, 60)
-text = font.render('py.zza game', True, (0, 255, 0))
+text_game = font.render('py.zza game', True, (0, 255, 0))
 font = pygame.font.SysFont(None, 34)
 score = font.render('score: ', True, (51, 51, 255))
 font = pygame.font.SysFont(None, 42)
-text2 = font.render('press any button to start', True, (255, 0, 0))
-image = pygame.image.load("imagens\\pizza.png").convert_alpha()
-image = pygame.transform.scale(image, (300, 300))
+text_press = font.render('press any button to start', True, (255, 0, 0))
+pizza_img = pygame.image.load("imagens\\pizza.png").convert_alpha()
+pizza_img = pygame.transform.scale(pizza_img, (350, 350))
 image2 = pygame.image.load("imagens\\cardapio.jpg").convert()
 image2 = pygame.transform.scale(image2, (xt,yt))
 image3 = pygame.image.load("imagens\\fundo.jpg").convert()
@@ -31,32 +34,58 @@ esteira = pygame.image.load("imagens\\esteira.jpg").convert()
 esteira = pygame.transform.scale(esteira,(1500,250))
 clock = pygame.time.Clock()
 fps = 240
-image4 = pygame.image.load("imagens\\pizzasr.png").convert_alpha()
-image4 = pygame.transform.scale(image4, (200, 200))
+pizzavazia_img = pygame.image.load("imagens\\pizzasr.png").convert_alpha()
+pizzavazia_img = pygame.transform.scale(pizzavazia_img, (200, 200))
 madeira = pygame.image.load("imagens\\madeira.png").convert_alpha()
 madeira = pygame.transform.scale(madeira,(1100,200))
 
-duende = pygame.image.load("imagens\\duende.png").convert_alpha()
-duende = pygame.transform.scale(duende, (75,75))
-barata = pygame.image.load("imagens\\barata1.png").convert_alpha()
-barata = pygame.transform.scale(barata, (75,75))
-sapo = pygame.image.load("imagens\\sapo.png").convert_alpha()
-sapo = pygame.transform.scale(sapo, (75,75))
-slime = pygame.image.load("imagens\\slime.png").convert_alpha()
-slime = pygame.transform.scale(slime, (75,120))
-zumbi = pygame.image.load("imagens\\zumbi.png").convert_alpha()
-zumbi = pygame.transform.scale(zumbi, (75,75))
-bota = pygame.image.load("imagens\\bota.png").convert_alpha()
-bota = pygame.transform.scale(bota, (75,75))
-chip = pygame.image.load("imagens\\chip.png").convert_alpha()
-chip = pygame.transform.scale(chip, (75,75))
-pimenta = pygame.image.load("imagens\\pimenta.png").convert_alpha()
-pimenta = pygame.transform.scale(pimenta, (75,120))
+duende_img = pygame.image.load("imagens\\duende.png").convert_alpha()
+duende_img = pygame.transform.scale(duende_img, (75,75))
+barata_img = pygame.image.load("imagens\\barata1.png").convert_alpha()
+barata_img = pygame.transform.scale(barata_img, (75,75))
+sapo_img = pygame.image.load("imagens\\sapo.png").convert_alpha()
+sapo_img = pygame.transform.scale(sapo_img, (75,75))
+slime_img = pygame.image.load("imagens\\slime.png").convert_alpha()
+slime_img = pygame.transform.scale(slime_img, (75,120))
+zumbi_img = pygame.image.load("imagens\\zumbi.png").convert_alpha()
+zumbi_img = pygame.transform.scale(zumbi_img, (75,75))
+bota_img = pygame.image.load("imagens\\bota.png").convert_alpha()
+bota_img = pygame.transform.scale(bota_img, (75,75))
+chip_img = pygame.image.load("imagens\\chip.png").convert_alpha()
+chip_img = pygame.transform.scale(chip_img, (75,75))
+pimenta_img = pygame.image.load("imagens\\pimenta.png").convert_alpha()
+pimenta_img = pygame.transform.scale(pimenta_img, (75,120))
 
 comanda = pygame.image.load("imagens\\comanda.png").convert_alpha()
 comanda = pygame.transform.scale(comanda, (250, 250))
 cursor = pygame.image.load("imagens\\cursor.png").convert_alpha()
 cursor = pygame.transform.scale(cursor, (20,20))
+
+all_sprites = pygame.sprite.Group()
+all_ingredientes = pygame.sprite.Group()
+
+ing = {
+    0: { "img": duende_img, "x": 330, "y": 267 },
+    1: { "img": barata_img, "x": 440, "y": 267 },
+    2: { "img": sapo_img, "x": 550, "y": 267 },
+    3: { "img": slime_img, "x": 200, "y": 230 },
+    4: { "img": zumbi_img, "x": 660, "y": 267 },
+    5: { "img": bota_img, "x": 770, "y": 267 },
+    6: { "img": chip_img, "x": 880, "y": 267 },
+    7: { "img": pimenta_img, "x": 100, "y": 230 }
+}
+
+lista_ingredientes = []
+
+for k, v in ing.items():
+    ingrediente = Ingrediente(v['img'], v['x'], v['y'])
+    lista_ingredientes.append(ingrediente)
+    all_ingredientes.add(ingrediente)
+    all_sprites.add(ingrediente)
+
+pizza = Pizza(pizzavazia_img, -100, 428)
+all_sprites.add(pizza)
+
 fallvel = 3
 cond = False
 cond2 = True
@@ -66,8 +95,9 @@ vel_esteira = 1
 x_esteira = -200
 x_esteira2 = -1700
 x_pizza = -250
+clock = pygame.time.Clock()
+selecionado = None
 while game:
-    clock = pygame.time.Clock()
     clock.tick(fps)
     mx, my = pygame.mouse.get_pos()
     # ----- Trata eventos
@@ -78,24 +108,34 @@ while game:
            
     if state == "start_screen":    
         window.blit(image2,(0,0))
-        window.blit(text, (420, 40))
-        window.blit(image,(400,150))
-        window.blit(text2, (380, 500))
+        window.blit(pizza_img,(355,150))
+        window.blit(text_game, (400, 40))
+        window.blit(text_press, (360, 500))
         if event.type == pygame.KEYUP:
             state = "game_screen"
     if state == "game_screen":
+        if selecionado != None:
+            selecionado.rect.x = mx
+            selecionado.rect.y = my
+        if event.type == pygame.MOUSEBUTTONDOWN and selecionado == None:
+            for i in range(len(lista_ingredientes)):
+                r = lista_ingredientes[i].rect
+                # print(r.x, r.right, r.y, r.bottom, mx, my)
+                if r.x <= mx and mx <= r.right and r.y <= my and my <= r.bottom:
+                    item = Ingrediente(ing[i]['img'], mx, my)
+                    selecionado = item
+                    all_sprites.add(selecionado)
+        if event.type == pygame.MOUSEBUTTONUP:
+            if selecionado != None:
+                r = pizza.rect
+                if r.x <= mx and mx <= r.right and r.y <= my and my <= r.bottom:
+                    pizza.add(selecionado)
+            selecionado = None
+
         window.blit(image3,(0,0))
         window.blit(esteira,(x_esteira,400))
         window.blit(esteira,(x_esteira2,400))
         window.blit(madeira,(0,200))
-        window.blit(duende,(330, 267))
-        window.blit(barata,(440, 267))
-        window.blit(sapo,(550, 267))
-        window.blit(slime,(100, 230))
-        window.blit(zumbi,(660,267))   
-        window.blit(bota,(770, 267))
-        window.blit(chip,(880, 267))
-        window.blit(pimenta,(200, 230))
         window.blit(comanda,(850, 0))
         window.blit(score, (870, 213))
         x_esteira += vel_esteira
@@ -105,113 +145,16 @@ while game:
             x_esteira = -1900
         if x_esteira2 >= 1100:
             x_esteira2 = -1900
-        window.blit(image4,(x_pizza,425))
+#        window.blit(image4,(x_pizza,425))
         if x_pizza > 1100:
             x_pizza = -200
+        
+        
         window.blit(cursor, ((mx), (my)))
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(duende,(330, 267)).collidepoint(mx,my):
-                    drag_duende = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_duende = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(barata,(440, 267)).collidepoint(mx,my):
-                    drag_barata = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_barata = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(sapo,(550, 267)).collidepoint(mx,my):
-                    drag_sapo = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_sapo = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(slime,(100, 230)).collidepoint(mx,my):
-                    drag_slime = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_slime = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(zumbi,(660, 267)).collidepoint(mx,my):
-                    drag_zumbi = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_zumbi = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(bota,(770, 267)).collidepoint(mx,my):
-                    drag_bota = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_bota = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(chip,(880, 267)).collidepoint(mx,my):
-                    drag_chip = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_chip = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if window.blit(pimenta,(200, 230)).collidepoint(mx,my):
-                    drag_pimenta = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                drag_pimenta = False
-                dropx, dropy = pygame.mouse.get_pos()
-                cond = True
                     
-        if drag_duende == True:
-            window.blit(duende,((mx-50),(my-50)))
-        elif drag_duende == False and cond == True: #(tentativa de fazer o ingrediente cair, mas fica spawnando duende toda vez que clica)
-             window.blit(duende,(x_pizza, 450))
-             #dropy += fallvel
-             #window.blit(duende,((dropx-10),(dropy-10)))
 
-        if drag_barata == True:
-            window.blit(barata,((mx-50),(my-50)))
-
-        if drag_sapo == True:
-            window.blit(sapo,((mx-50),(my-50)))
-
-        if drag_slime == True:
-            window.blit(slime,((mx-50),(my-50)))
-
-        if drag_zumbi == True:
-            window.blit(zumbi,((mx-50),(my-50)))
-
-        if drag_bota == True:
-            window.blit(bota,((mx-50),(my-50)))
-
-        if drag_chip == True:
-            window.blit(chip,((mx-50),(my-50)))
-
-        if drag_pimenta == True:
-            window.blit(pimenta,((mx-50),(my-50)))
-
-
-
+        all_sprites.update()
+        all_sprites.draw(window)
 
     # Constructor. Pass in the color of the block,
     # and its x and y position
@@ -230,5 +173,7 @@ while game:
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+
 
  
---
+++ sprites.py
@@ -0,0 +1,36 @@
+import pygame
+from config import *
+
+class Ingrediente(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speedx = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+
+
+class Pizza(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.ingredientes = pygame.sprite.Group()
+        self.speedx = 1
+
+    def add(self, ingrediente):
+        self.ingredientes.add(ingrediente)
+        ingrediente.speedx = self.speedx
+
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.x > xt:
+            for i in self.ingredientes.sprites():
+                i.rect.x = i.rect.x - self.rect.x - 200
+            self.rect.x = -200
